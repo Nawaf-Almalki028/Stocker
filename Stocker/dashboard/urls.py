b8785c046fe2f51e4a0cdb8057a93c9e81e983bb
Nawaf Kamal <xp.mo91@gmail.com>
2025-08-10 20:18:00 +0300

    categories and prod pages and more

--- Stocker/dashboard/urls.py
+++ Stocker/dashboard/urls.py
@@ -6,5 +6,7 @@ app_name = "dashboard"
 
 urlpatterns = [
     path('', views.dashboard_home, name="dashboard_home"),
+    path('products/', views.dashboard_prod, name="dashboard_prod"),
     path('suppliers/', views.dashboard_suppliers, name="dashboard_supplier"),
+    path('categories/', views.dashboard_categ, name="dashboard_categ"),
 ]
--- Stocker/dashboard/views.py
+++ Stocker/dashboard/views.py
@@ -4,5 +4,11 @@ from django.http import HttpRequest,HttpResponse
 def dashboard_home(request:HttpRequest):
   return render(request, "main/dash_home.html")
 
+def dashboard_categ(request:HttpRequest):
+  return render(request, "main/categ_home.html")
+
+def dashboard_prod(request:HttpRequest):
+  return render(request, "main/prod_home.html")
+
 def dashboard_suppliers(request:HttpRequest):
   return render(request, "products.html")
